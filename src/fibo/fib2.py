c94354503b473842b65df5c102dfe02d26de8bea
Arttu Valo <arttu.valo@gmail.com>
2020-10-09 20:23:01 +0300

    Configration changes, added tests.

--- src/fibo/fib2.py
+++ src/fibo/fib2.py
@@ -3,5 +3,5 @@ def fib2(n):   # return Fibonacci series up to n
     a, b = 0, 1
     while a < n:
         result.append(a)
-        a, b = b, a+b
+        a, b = b, a + b
     return result
---
+++ tests/test_fibo.py
@@ -0,0 +1,12 @@
+import unittest
+
+from fibo.fib2 import fib2
+
+
+class TestFibo(unittest.TestCase):
+    def test_fib2(self):
+        self.assertEqual(fib2(10), [1, 1, 2, 3, 5, 8])
+
+
+if __name__ == '__main__':
+    unittest.main()
