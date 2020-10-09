233bea6aed384a71086be778e9ea34426146ccbd
Arttu Valo <arttu.valo@gmail.com>
2020-10-09 20:29:12 +0300

    Fixed broken test

--- tests/test_fibo.py
+++ tests/test_fibo.py
@@ -5,7 +5,7 @@ from fibo.fib2 import fib2
 
 class TestFibo(unittest.TestCase):
     def test_fib2(self):
-        self.assertEqual(fib2(10), [1, 1, 2, 3, 5, 8])
+        self.assertEqual(fib2(10), [0, 1, 1, 2, 3, 5, 8])
 
 
 if __name__ == '__main__':
